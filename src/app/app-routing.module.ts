669e5d076f8ba910b49f4278fb2d7b1b71c1ec95
Brandao <ana.brandao@capgemini.com>
2022-04-28 07:26:40 -0300

    Resolvendo navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroServicoComponent } from './cadastro-servico/cadastro-servico.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -7,7 +9,15 @@ const routes: Routes = [
   {
     path:'login',
     component: LoginComponent
-  }
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'cadservico',
+    component: CadastroServicoComponent
+  },
 
 ];
 
