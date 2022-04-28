a0c537bc720700c5f0a11ce6e34acb63238cf1e4
Brandao <ana.brandao@capgemini.com>
2022-04-28 12:04:06 -0300

    Add repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
+    path:'',
+    component: HomeComponent
+  },
+  {
     path:'cadservico',
     component: CadastroServicoComponent
   },
