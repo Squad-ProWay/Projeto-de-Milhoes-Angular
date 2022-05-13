a6ea5bae15d77ee9ce18821475013d7e426ff59b
BrandaoSt <patriciabrandao7@gmail.com>
2022-05-13 13:35:59 -0300

    add authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -67,7 +67,7 @@ const routes: Routes = [
   {
     path:'cadhorario',
     component: CadastroHorarioComponent,
-    canActivate: [AuthAdmService]
+    canActivate: [AuthLoginService, AuthAdmService]
   },
   {
     path:'cadusuario',
@@ -75,8 +75,7 @@ const routes: Routes = [
   },
   {
     path:'servicos',
-    component: InfoServicosComponent,
-    canActivate: [AuthAdmService]
+    component: InfoServicosComponent
   },
   {
     path:'sobre',
