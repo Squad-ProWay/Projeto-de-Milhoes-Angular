3dd0b936de6d1d250cead661ca965f31b239f0a9
Luara Gesteira Silva Sousa <luagesteira@gmail.com>
2022-06-20 15:31:29 -0300

    ok

--- src/app/cadastro-funcionario/cadastro-funcionario.component.ts
+++ src/app/cadastro-funcionario/cadastro-funcionario.component.ts
@@ -9,6 +9,29 @@ import { FuncionariosService } from '../funcionarios.service';
 export class CadastroFuncionarioComponent implements OnInit {
 
   constructor(private serviceFuncionario: FuncionariosService) { }
+  validCpf: boolean = false;
+
+
+  validateCPF(cpf: string) {
+    let Soma = 0;
+    let Resto;
+
+    for (let i = 1; i <= 9; i++)
+      Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto === 10 || Resto === 11) Resto = 0;
+    if (Resto !== parseInt(cpf.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (let i = 1; i <= 10; i++)
+      Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11))) return false;
+    return true;
+  }
 
   gravar(dados: any){
     this.serviceFuncionario.gravar(dados).subscribe(x => window.location.reload())
