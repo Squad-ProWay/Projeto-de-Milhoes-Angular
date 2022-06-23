a3576a0608c17048206337456a22edb0803dfd1b
Patricia Brandão <patriciabrandao7@gmail.com>
2022-06-23 10:32:34 -0300

    Update edita-horario.component.ts

--- src/app/edita-horario/edita-horario.component.ts
+++ src/app/edita-horario/edita-horario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FuncionariosService } from '../funcionarios.service';
 import { HorarioService } from '../horario.service';
+import { ServicosService } from '../servicos.service';
 
 @Component({
   selector: 'app-edita-horario',
@@ -9,15 +11,19 @@ import { HorarioService } from '../horario.service';
 })
 export class EditaHorarioComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private horarioService: HorarioService) { }
+  constructor(private route: ActivatedRoute, private horarioService: HorarioService, private serviceServico: ServicosService, private serviceFuncionario: FuncionariosService) { }
 
   horario: any = {}
+  servicos: any = {}
+  funcionarios: any = {}
   msg: string = ''
 
   ngOnInit(): void {
     let routeParams = this.route.snapshot.paramMap;
     let idhorario: number = Number(routeParams.get('idhorario'))
     this.horarioService.getOne(idhorario).subscribe(x => this.horario = x)
+    this.serviceServico.getAll().subscribe(x => this.servicos = x)
+    this.serviceFuncionario.getAll().subscribe(x => this.funcionarios = x)
   }
 
   efetivarAlteracao() {
